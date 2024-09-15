42b4c6499b96ee46fcc91fd03e05f6c95b23e405
Eudes Alves <eudesnoberto@hotmail.com>
2024-09-15 14:46:22 -0300

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,19 +2,32 @@ CREATE DATABASE associacao;
 
 USE associacao;
 
--- Tabela de usuários
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(100) NOT NULL,
-    tipo ENUM('morador', 'admin') DEFAULT 'morador'  -- O admin será o presidente
+    tipo ENUM('morador', 'admin') DEFAULT 'morador'
 );
 
--- Tabela de notificações/avisos
 CREATE TABLE avisos (
     id INT AUTO_INCREMENT PRIMARY KEY,
     titulo VARCHAR(100) NOT NULL,
     mensagem TEXT NOT NULL,
     data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE agendamentos_comprovantes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    data_comprovante DATE NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+);
+
+CREATE TABLE requisicoes_espaco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    usuario_id INT NOT NULL,
+    data_evento DATE NOT NULL,
+    descricao_evento TEXT NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
