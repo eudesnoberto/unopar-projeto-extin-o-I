36514cf36006f4023a2844c8f8dae844d444012b
Eudes Alves <eudesnoberto@hotmail.com>
2024-09-15 14:40:16 -0300

    Update db.php

--- db.php
+++ db.php
@@ -8,6 +8,6 @@ try {
     $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo 'Erro de conexão: ' . $e->getMessage();
+    die("Não foi possível conectar ao banco de dados: " . $e->getMessage());
 }
 ?>
